81bec604ec61409fd38c47b906e1f4c50c5249fe
Julian Gentges <JulGentges@gmail.com>
2019-05-24 13:05:48 +0200

    updated vapor to latest release and auth to latest

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporOAuth",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-    	.Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
+    	.Package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+    	.Package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
     ]
 )
