16fdbae23fabe0e6509e17e763be426b1d625ac7
pmacro <34399637+pmacro@users.noreply.github.com>
2019-08-22 13:00:51 -0500

    Update Package.swift
    Update syntax

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporOAuth",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
-    	.Package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
+    	.package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+    	.package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
     ]
 )
