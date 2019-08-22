e8c5d6e146f42f8993d52b7bd5323fbf0c022a71
pmacro <34399637+pmacro@users.noreply.github.com>
2019-08-22 13:09:47 -0500

    Update Package.swift
    Add targets

--- Package.swift
+++ Package.swift
@@ -7,5 +7,9 @@ let package = Package(
     dependencies: [
     	.package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
     	.package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
+    ],
+    targets: [
+      .target(name: "VaporOAuth", dependencies: ["Vapor", "AuthProvider"]),
+      .testTarget(name: "VaporOAuthTests", dependencies: ["VaporOAuth"]),
     ]
 )
