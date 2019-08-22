2de42555178eeccfd86a8f92077d7dd943a36ebf
pmacro <34399637+pmacro@users.noreply.github.com>
2019-08-22 13:19:09 -0500

    Update Package.swift
    Add products

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "VaporOAuth",
+    products: [
+        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
+    ],
     dependencies: [
     	.package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
     	.package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
